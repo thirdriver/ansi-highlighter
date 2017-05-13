0915b8aec3d9b90cc2f52fd4c5c940bf27e0f098
Ahmed Layouni <layouni.ahmed86@gmail.com>
2017-05-14 00:30:46 +0100

    Fixed reset bug

---
+++ src/com/alayouni/ansihighlight/ANSI.java
@@ -0,0 +1,266 @@
+package com.alayouni.ansihighlight;
+
+import com.intellij.codeInsight.highlighting.HighlightManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.markup.EffectType;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by alayouni on 5/12/17.
+ */
+public class ANSI {
+    private static final String ESC = "\u001B[";
+    private static final char SEQ_END = 'm';
+    private static final char SEQ_DELIM = ';';
+
+    private static final int RESET = 0;
+
+    private static final int BOLD = 1;
+    private static final int ITALIC = 3;
+    private static final List<Integer> fontCodeToType = new ArrayList<>();
+    static {
+        fontCodeToType.add(null);
+        fontCodeToType.add(Font.BOLD);
+        fontCodeToType.add(null);
+        fontCodeToType.add(Font.ITALIC);
+    }
+    private static final Map<Integer, Integer> fontTypeToCode = new HashMap<>();
+    static {
+        fontTypeToCode.put(Font.BOLD, BOLD);
+        fontTypeToCode.put(Font.ITALIC, ITALIC);
+    }
+
+    private static final int UNDERLINE = 4;
+
+    private static final int BLACK = 30;
+    private static final int RED	 = 31;
+    private static final int GREEN = 32;
+    private static final int YELLOW = 33;
+    private static final int BLUE = 34;
+    private static final int MAGENTA = 35;
+    private static final int CYAN = 36;
+    private static final int WHITE = 37;
+    private static final List<Color> codeToForeground = new ArrayList<>();
+    static {
+        for(int i = 0; i < BLACK; i ++) {
+            codeToForeground.add(null);
+        }
+        codeToForeground.add(Color.BLACK);
+        codeToForeground.add(Color.RED);
+        codeToForeground.add(Color.GREEN);
+        codeToForeground.add(Color.YELLOW);
+        codeToForeground.add(Color.BLUE);
+        codeToForeground.add(Color.MAGENTA);
+        codeToForeground.add(Color.CYAN);
+        codeToForeground.add(Color.LIGHT_GRAY);
+    }
+    private static final Map<Color, Integer> foregroundToCode = new HashMap<>();
+    static {
+        for(int i = BLACK; i <= WHITE; i++) {
+            foregroundToCode.put(codeToForeground.get(i), i);
+        }
+    }
+
+    private static final int BLACK_BG = 40;
+    private static final int RED_BG = 41;
+    private static final int GREEN_BG = 42;
+    private static final int YELLOW_BG = 43;
+    private static final int BLUE_BG = 44;
+    private static final int MAGENTA_BG = 45;
+    private static final int CYAN_BG = 46;
+    private static final int WHITE_BG = 47;
+    //might be used in future to save marked documents in ANSI format
+    private static final List<Color> codeToBackground = new ArrayList<>();
+    static {
+        for(int i = 0; i < BLACK_BG; i++) {
+            codeToBackground.add(null);
+        }
+        codeToBackground.add(Color.BLACK);
+        codeToBackground.add(Color.RED);
+        codeToBackground.add(Color.GREEN);
+        codeToBackground.add(Color.YELLOW);
+        codeToBackground.add(Color.BLUE);
+        codeToBackground.add(Color.MAGENTA);
+        codeToBackground.add(Color.CYAN);
+        codeToBackground.add(Color.LIGHT_GRAY);
+    }
+    //might be used in future to save marked documents in ANSI format
+    private static final Map<Color, Integer> backgroundToCode = new HashMap<>();
+    static {
+        for(int i = BLACK_BG; i <= WHITE_BG; i++) {
+            backgroundToCode.put(codeToBackground.get(i), i);
+        }
+    }
+
+    private static final TextAttributes RESET_MARKER = new TextAttributes();
+
+    private final HighlightManager highlighter;
+
+    public ANSI(Project project) {
+        this.highlighter = HighlightManager.getInstance(project);
+    }
+
+    public Pair<List<RangedAttributes>, String> extractAttributesFromAnsiTaggedText(String text) {
+        List<RangedAttributes> highlights = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int escIndex = text.indexOf(ESC),
+                startIndex = 0;
+        if(escIndex == -1) return new Pair<>(highlights, text);
+        RangedAttributes range = null;
+        Pair<TextAttributes, Integer> attr;
+        while (escIndex >= 0) {
+            sb.append(text.substring(startIndex, escIndex));
+            attr = extractTextAttributesFromANSIEscapeSequence(text, escIndex);
+            if(attr == null) {
+                startIndex = escIndex;
+                escIndex = text.indexOf(ESC, escIndex + 1);
+            } else {
+                if(range != null) {
+                    range.end = sb.length();
+                    highlights.add(range);
+                }
+                startIndex = attr.second;
+                if(startIndex == text.length()) break;
+
+                if(attr.first == RESET_MARKER) range = null;
+                else {
+                    range = new RangedAttributes();
+                    range.start = sb.length();
+                    range.attr = attr.first;
+                }
+                escIndex = text.indexOf(ESC, startIndex);
+            }
+            if(escIndex == -1) {
+                sb.append(text.substring(startIndex, text.length()));
+                if(range != null) {
+                    range.end = sb.length();
+                    highlights.add(range);
+                }
+            }
+        }
+        return new Pair<>(highlights, sb.toString());
+    }
+
+    public void applyHighlightsToEditor(List<RangedAttributes> highlights, Editor editor) {
+        editor.getMarkupModel().removeAllHighlighters();
+        for(RangedAttributes range : highlights) {
+            highlighter.addRangeHighlight(editor, range.start, range.end, range.attr, false, null);
+        }
+    }
+
+    private Pair<TextAttributes, Integer> extractTextAttributesFromANSIEscapeSequence(String text, int escIndex) {
+        List<Integer> ansiCodes = new ArrayList<>();
+        TextAttributes attr = new TextAttributes();
+        int endIndex = -1, index, startIndex;
+        int ansiCode;
+        char c;
+        root: while (escIndex < text.length() && text.startsWith(ESC, escIndex)) {
+            ansiCodes.clear();
+            startIndex = index = escIndex + ESC.length();
+            do {
+                while (index < text.length() && isDigit(text.charAt(index))) index++;
+                if (index == text.length()) break root;
+                c = text.charAt(index);
+                if (c == SEQ_DELIM || c == SEQ_END) {
+                    ansiCode = toAnsiCodeIfSupported(text.substring(startIndex, index));
+                    if (ansiCode == -1) break root;
+                    ansiCodes.add(ansiCode);
+                    startIndex = ++index;
+                    if (c == SEQ_END) {
+                        endIndex = escIndex = index;
+                        attr = applyCodes(ansiCodes, attr);
+                    }
+                } else break root;
+            } while(c == SEQ_DELIM);
+        }
+        if(endIndex == -1) return null;
+        return new Pair<>(attr, endIndex);
+    }
+
+    private TextAttributes applyCodes(List<Integer> ansiCodes, TextAttributes attr) {
+        for(int code : ansiCodes) {
+            attr = applySupportedCode(code, attr);
+        }
+        if(attr.getEffectType() == EffectType.LINE_UNDERSCORE || attr.getEffectType() == EffectType.BOLD_LINE_UNDERSCORE) {//if so this is not RESET_MARKER
+            if((attr.getFontType() & Font.BOLD) == Font.BOLD) {
+                attr.setEffectType(EffectType.BOLD_LINE_UNDERSCORE);
+            } else {
+                attr.setEffectType(EffectType.LINE_UNDERSCORE);
+            }
+        }
+        return attr;
+    }
+
+    private TextAttributes applySupportedCode(int code, TextAttributes attr) {
+        if(code == RESET) return RESET_MARKER;
+        if(attr == RESET_MARKER) attr = new TextAttributes();
+        if(code == UNDERLINE) attr.setEffectType(EffectType.LINE_UNDERSCORE);
+        else {
+            int fontType = fontTypeFromCode(code);
+            if(fontType != -1) {
+                attr.setFontType(attr.getFontType() | fontType);
+            } else {
+                Color foreground = foregroundFromCode(code);
+                if(foreground != null) {
+                    attr.setForegroundColor(foreground);
+                } else {
+                    Color background = backgroundFromCode(code);
+                    attr.setBackgroundColor(background);
+                }
+            }
+        }
+        return attr;
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private int toAnsiCodeIfSupported(String ansiCodeStr) {
+        try {
+            int code = Integer.parseInt(ansiCodeStr);
+            return isAnsiCodeSupported(code) ? code : -1;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    private boolean isAnsiCodeSupported(int ansiCode) {
+        if(ansiCode == RESET || ansiCode == UNDERLINE) return true;
+        if(fontTypeFromCode(ansiCode) != -1) return true;
+        if(foregroundFromCode(ansiCode) != null) return true;
+        if(backgroundFromCode(ansiCode) != null) return true;
+        return false;
+    }
+
+    private int fontTypeFromCode(int fontTypeCode) {
+        if(fontTypeCode >= fontCodeToType.size()) return -1;
+        Integer fontType = fontCodeToType.get(fontTypeCode);
+        return fontType == null ? -1 : fontType;
+    }
+
+    private Color foregroundFromCode(int foregroundCode) {
+        if(foregroundCode >= codeToForeground.size()) return null;
+        return codeToForeground.get(foregroundCode);
+    }
+
+    private Color backgroundFromCode(int backgroundCode) {
+        if(backgroundCode >= codeToBackground.size()) return null;
+        return codeToBackground.get(backgroundCode);
+    }
+
+
+    public static class RangedAttributes {
+        public int start, end;
+        public TextAttributes attr;
+    }
+
+}
--- src/com/alayouni/ansihighlight/ANSIHighlighterComponent.java
+++ src/com/alayouni/ansihighlight/ANSIHighlighterComponent.java
@@ -1,13 +1,11 @@
 package com.alayouni.ansihighlight;
 
 import com.intellij.AppTopics;
-import com.intellij.codeInsight.highlighting.HighlightManager;
 import com.intellij.conversion.CannotConvertException;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.*;
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
 import com.intellij.openapi.fileEditor.impl.EditorWindow;
@@ -15,6 +13,7 @@ import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupManager;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.StandardFileSystems;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
@@ -22,9 +21,9 @@ import com.intellij.testFramework.LightVirtualFile;
 import com.intellij.util.messages.MessageBus;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,6 +35,7 @@ public class ANSIHighlighterComponent implements ProjectComponent, ANSIHighlight
         final String realFilePath, collapsedRealFilePath, lightFileName;
         OpenFileDescriptor lightFileDescriptor;
         int openEditorCount = 0;
+
 
         public OpenLightFileInfo(VirtualFile realFile, String lightFileName) {
             this.realFilePath = realFile.getPath();
@@ -52,9 +52,11 @@ public class ANSIHighlighterComponent implements ProjectComponent, ANSIHighlight
     private Editor lastSelectedEditor;
     private VirtualFile lastSelectedFile;
     private boolean isTogglingANSIHighlighter = false;
+    private final ANSI ansi;
 
     public ANSIHighlighterComponent(Project project) {
         this.project = project;
+        this.ansi = new ANSI(project);
         lightFileToReal = new HashMap<>();
         realFileToLight = new HashMap<>();
         try {
@@ -123,28 +125,14 @@ public class ANSIHighlighterComponent implements ProjectComponent, ANSIHighlight
     }
 
     private void lightFileOpened(@NotNull FileEditorManager fileEditorManager, VirtualFile file, OpenLightFileInfo info) {
-        utils.runInEDT(() -> {
-            Editor editor = ((TextEditor) fileEditorManager.getSelectedEditor(file)).getEditor();
-            info.lightFileDescriptor.navigate(false);
-            editor.getMarkupModel().removeAllHighlighters();
-
-            TextAttributes attr = new TextAttributes(Color.red, null, null, null, Font.PLAIN);
-            TextAttributes attr2 = new TextAttributes(Color.orange, null, null, null, Font.PLAIN);
-            HighlightManager.getInstance(project).addRangeHighlight(editor, 0, 4, attr, false, null);
-            HighlightManager.getInstance(project).addRangeHighlight(editor, 6, 10, attr2, false, null);
-        }, false);
-
-//                    editor.getDocument().addDocumentListener(new DocumentAdapter() {
-//                        @Override
-//                        public void documentChanged(DocumentEvent e) {
-//                            long fileModification = file.getModificationStamp(),
-//                                    documentStamp = e.getDocument().getModificationStamp();
-//                            String cmp = cmp(fileModification, documentStamp);
-//
-//                            System.out.println("After: fileName=" + file.getName() + "   file modi: " + fileModification + "-" + fileModification + cmp + " document modification: " + documentStamp);
-//                        }
-//                    });
-
+//        utils.runInEDT(() -> {
+//            Editor editor = ((TextEditor) fileEditorManager.getSelectedEditor(file)).getEditor();
+//            HighlightManager highlighter = HighlightManager.getInstance(project);
+//            editor.getMarkupModel().removeAllHighlighters();
+//            for(ANSI.RangedAttributes marker : info.highlights) {
+//                highlighter.addRangeHighlight(editor, marker.start, marker.end, marker.attr, false, null);
+//            }
+//        }, false);
     }
 
     /**
@@ -157,16 +145,16 @@ public class ANSIHighlighterComponent implements ProjectComponent, ANSIHighlight
      * @param associatedLightFileInfo
      */
     private void realFileOpenedFirstTime(@NotNull FileEditorManager fileEditorManager, @NotNull VirtualFile file, OpenLightFileInfo associatedLightFileInfo) {
-        //todo remove below
-        String fileContent = file.getName();
-        fileContent += (fileContent + "   " + fileContent + "   " + fileContent + "   " + fileContent + "   " + fileContent + "   " + fileContent + "   " + fileContent + "   ");
-        //todo remove above
+        Editor editor = ((TextEditor) fileEditorManager.getSelectedEditor(file)).getEditor();
+        String fileContent = editor.getDocument().getText();
+        Pair<List<ANSI.RangedAttributes>, String> highlighted = ansi.extractAttributesFromAnsiTaggedText(fileContent);
 
         FileType fileType = file.getFileType();
-        VirtualFile lightFile = new LightVirtualFile(associatedLightFileInfo.lightFileName, fileType, fileContent, file.getModificationStamp());
+        VirtualFile lightFile = new LightVirtualFile(associatedLightFileInfo.lightFileName, fileType, highlighted.second, file.getModificationStamp());
         associatedLightFileInfo.lightFileDescriptor = new OpenFileDescriptor(project, lightFile);
         utils.runInEDT(() -> {
-            fileEditorManager.openTextEditor(associatedLightFileInfo.lightFileDescriptor, true);
+            Editor lightEditor = fileEditorManager.openTextEditor(associatedLightFileInfo.lightFileDescriptor, true);
+            ansi.applyHighlightsToEditor(highlighted.first, lightEditor);
             fileEditorManager.closeFile(file);//safe to close using file reference since here file got opened for the first time
         }, !isProjectInitialized);
     }
@@ -189,7 +177,9 @@ public class ANSIHighlighterComponent implements ProjectComponent, ANSIHighlight
      * @param associatedLightFileInfo
      */
     private void realFileReOpened(@NotNull FileEditorManager fileEditorManager, @NotNull VirtualFile file, OpenLightFileInfo associatedLightFileInfo) {
-//        Editor editor = ((TextEditor) fileEditorManager.getSelectedEditor(file)).getEditor();
+        Editor editor = ((TextEditor) fileEditorManager.getSelectedEditor(file)).getEditor();
+        String fileContent = editor.getDocument().getText();
+        Pair<List<ANSI.RangedAttributes>, String> highlighted = ansi.extractAttributesFromAnsiTaggedText(fileContent);
         utils.runInEDT(() -> {
             EditorWindow window = ((FileEditorManagerEx)fileEditorManager).getSplitters().getCurrentWindow();
             if(this.lastSelectedEditor != null && isProjectInitialized) {
@@ -213,18 +203,37 @@ public class ANSIHighlighterComponent implements ProjectComponent, ANSIHighlight
                 //here the opening is triggered by user => there's no chance the real file got opened in a new splitter => the real file is not alone in the splitter
                 //=> closing the real file editor first is safe since it won't cause an empty splitter
                 fileEditorManager.closeFile(file);
-                fileEditorManager.openTextEditor(associatedLightFileInfo.lightFileDescriptor, true);
+                boolean fileWasOpen = fileEditorManager.isFileOpen(associatedLightFileInfo.lightFileDescriptor.getFile());
+                Editor lightEditor = fileEditorManager.openTextEditor(associatedLightFileInfo.lightFileDescriptor, true);
+                if(!fileWasOpen) {
+                    ansi.applyHighlightsToEditor(highlighted.first, lightEditor);
+                }
             } else {
                 //triggered during project initialization => the real file can get opened in a new splitter => there's a chance the
                 //the real file editor would be alone under its splitter => closing the real file editor first might result in an empty splitter, which
                 //causes bug => opening the associated light file before closing the real file would prevent an empty splitter
                 //=> reporting the closing of the real file is not an issue here (during initialization) since the caret/focus end position is not a priority
-                ((FileEditorManagerImpl) fileEditorManager).openFileImpl2(window, associatedLightFileInfo.lightFileDescriptor.getFile(), false);
-
+                VirtualFile lightFile = associatedLightFileInfo.lightFileDescriptor.getFile();
+                FileEditorManagerImpl fem = (FileEditorManagerImpl) fileEditorManager;
+                Pair<FileEditor[], FileEditorProvider[]> lightEditors = fem.openFileImpl2(window, lightFile, false);
+                highlightTextEditor(lightEditors.first, highlighted.first);
                 fileEditorManager.closeFile(file);
             }
         }, !isProjectInitialized);
     }
+
+    private void highlightTextEditor(FileEditor[] fileEditors, List<ANSI.RangedAttributes> highlights) {
+        for(FileEditor fileEditor : fileEditors) {
+            if(fileEditor instanceof TextEditor) {
+                ansi.applyHighlightsToEditor(highlights, ((TextEditor)fileEditor).getEditor());
+                return;
+            } else if(fileEditor instanceof Editor) {
+                ansi.applyHighlightsToEditor(highlights, (Editor)fileEditor);
+                return;
+            }
+        }
+    }
+
 
     private OpenLightFileInfo infoForRealFile(VirtualFile realFile) {
         String realPath = realFile.getPath();
@@ -264,7 +273,8 @@ public class ANSIHighlighterComponent implements ProjectComponent, ANSIHighlight
             }
 
             isTogglingANSIHighlighter = true;
-            ((FileEditorManagerImpl)fem).openFileImpl2(window, fileToOpen, true);
+            Pair<FileEditor[], FileEditorProvider[]> lightEditors = ((FileEditorManagerImpl)fem).openFileImpl2(window, fileToOpen, true);
+
             window.closeFile(file);
         }, false);
     }
